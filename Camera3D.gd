5ff38c4c60d537a943485d8a218cbd7e3c1b72b5
IvanSuharev <117433644+IvanSuharev@users.noreply.github.com>
2023-03-12 14:01:00 +0800

    Dance

--- Camera3D.gd
+++ Camera3D.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-
+const speed = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +9,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_up"):
+	_contoller()
+		
+
+func _contoller():
+	if Input.is_action_pressed("Focus+"):
 		fov -= 1
-	elif Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("Focus-"):
 		fov += 1
+	if Input.is_action_pressed("ui_right"):
+		rotation.y += -speed
+	if Input.is_action_pressed("ui_left"):
+		rotation.y += speed
+	if Input.is_action_pressed("ui_up"):
+		rotation.x += speed
+	if Input.is_action_pressed("ui_down"):
+		rotation.x += -speed
 
