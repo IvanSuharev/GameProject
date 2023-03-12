d0e9e4ecfce0a60cd6f2c2f1b4654cc9111b43a4
IvanSuharev <117433644+IvanSuharev@users.noreply.github.com>
2023-03-12 18:54:51 +0800

    create player

--- Scripts/Camera3D.gd
+++ Scripts/Camera3D.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 
-const speed = 0.01
-
+const speed = 0.05
+const move_speed = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -9,14 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_contoller()
+	look_at($"../".position, Vector3(0, 1, 0))
 		
 
 func _contoller():
 	if Input.is_action_pressed("Focus+"):
-		fov -= 0.05
+		fov -= 1
 	if Input.is_action_pressed("Focus-"):
-		fov += 0.05
+		fov += 1
 	if Input.is_action_pressed("ui_right"):
 		rotation.y += -speed
 	if Input.is_action_pressed("ui_left"):
@@ -25,4 +25,6 @@ func _contoller():
 		rotation.x += speed
 	if Input.is_action_pressed("ui_down"):
 		rotation.x += -speed
+		
+
 
--- Scripts/MeshInstance3D2.gd
+++ Scripts/MeshInstance3D2.gd
@@ -8,24 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_contoller()
-	rotation.x += speed
-	rotation.y += speed
-	rotation.z += speed
+	rotation.x += speed 
+	rotation.y += speed 
+	rotation.z += speed 
 	
 	
-func _contoller():
-	if Input.is_action_pressed("forward"):
-		translate(Vector3(-0.1, 0, 0))
-	if Input.is_action_pressed("back"):
-		translate(Vector3(0.1, 0, 0))
-	if Input.is_action_pressed("down"):
-		translate(Vector3(0, -0.1, 0))
-	if Input.is_action_pressed("up"):
-		translate(Vector3(0, 0.1, 0))
-	if Input.is_action_pressed("right"):
-		translate(Vector3(0, 0, -0.1))
-	if Input.is_action_pressed("left"):
-		translate(Vector3(0, 0, 0.1))
-
 
