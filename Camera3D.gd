a60d0a91a40c2b8c4d8bba46306e30a3cb1b81ab
Krakaturka <surikovm666@mail.ru>
2023-03-12 14:27:50 +0800

    Main menu

--- Camera3D.gd
+++ Camera3D.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-const speed = 0.05
+const speed = 0.01
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +14,9 @@ func _process(delta):
 
 func _contoller():
 	if Input.is_action_pressed("Focus+"):
-		fov -= 1
+		fov -= 0.05
 	if Input.is_action_pressed("Focus-"):
-		fov += 1
+		fov += 0.05
 	if Input.is_action_pressed("ui_right"):
 		rotation.y += -speed
 	if Input.is_action_pressed("ui_left"):
---
+++ Scripts/Node2D.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	get_tree().change_scene("res://Scenes/settings.tscn")
+
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
+
+
+func _on_settings_pressed():
+	get_tree().change_scene("res://Scenes/settings.tscn")
